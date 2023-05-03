f99a6a0378e5dc63cc4f5ff7a183f7399b53366c
Notaginger359 <leon.goergen@gmail.com>
2023-05-03 08:52:46 +0200

    working on sections "Vorschau", "Top10", ...

--- app.js
+++ app.js
@@ -12,11 +12,26 @@ const puppeteer = require('puppeteer');
     // Navigate to the website
     await page.goto('https://www.cinestar.de/kino-rostock-capitol');
 
-    // Wait for the movie titles to be loaded
-    await page.waitForSelector('.title');
+    async function processMovieTitles(page) {
+        // Find all div elements with the class "swiper-slide"
+        const divElements = await page.$$eval('div.swiper-slide', elements => {
+            return elements.filter(element => {
+                // Check if the div element has only one child element which is a span element
+                return element.childNodes.length === 1 && element.childNodes[0].nodeName.toLowerCase() === 'span';
+            }).map(element => element.outerHTML);
+        });
 
-    // Get the movie titles
-    const movieTitles = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim()));
+        // Loop through each div element and click on it
+        for (const divElement of divElements) {
+            await page.setContent(divElement);
+            await page.click('span');
+            await page.waitForSelector('.title');
+
+            // Get the movie titles
+            const movieTitles = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim()));
+            console.log(movieTitles);
+        }
+    }
 
     // Clean the titles from "Neu:" and empty strings
     const cleanTitles = movieTitles.map(title => title.replace('Neu:', ''));
@@ -33,6 +48,7 @@ const puppeteer = require('puppeteer');
         );
     });
     console.log(uniqueTitles);
+    console.log(uniqueTitles.length)
 
     // Close the browser
     await browser.close();
