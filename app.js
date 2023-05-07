c6a5cf4726490c3622ca866062b7c334eaed120a
Notaginger359 <leon.goergen@gmail.com>
2023-05-07 11:59:04 +0200

    another approach to click buttons

--- app.js
+++ app.js
@@ -14,6 +14,29 @@ const {ConsoleMessage} = require("puppeteer");
     // Try to get the buttons "Woche", "Heute", "Top10", ...
     const elements = await page.$$('.swiper-slide');
 
+    try {
+    // Click on each selection button and wait for the HTML to become visible
+    const selectionTypes = ['Woche', 'Heute', 'Vorverkauf', 'Top 10', 'Events', 'Vorschau'];
+    const swiperSlides = await page.$$('.ShowFilterView .swiper-slide');
+    for (let i = 0; i < swiperSlides.length; i++) {
+        const span = await swiperSlides[i].$('span');
+        const spanText = await page.evaluate(el => el.textContent, span);
+        console.log(spanText);
+        if (selectionTypes.includes(spanText)) {
+            await page.waitForTimeout(2000);
+            console.log(`Clicking on ${swiperSlides[i]} button`);
+            await swiperSlides[i].click();
+            console.log(`Clicked on ${swiperSlides[i]} button`);
+            await page.waitForSelector(`.ShowFilterView.${spanText}`, { visible: true, timeout: 5000 });
+
+            const currentMovies = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim()));
+            console.log(currentMovies);
+        }
+    }
+    } catch (error) {
+        console.error(error);
+    }
+
     const movieTitles = []
     for (let i = 0; i < elements.length; i++) {
         await elements[i].click(); // Button click does not seem to work
@@ -22,35 +45,8 @@ const {ConsoleMessage} = require("puppeteer");
         // Get the movie titles
         const currentMovies = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim()));
         movieTitles.push(...currentMovies);
-        console.log(currentMovies);
+        //console.log(currentMovies);
     }
-
-//    async function processMovieTitles(page) {
-//        // Find all div elements with the class "swiper-slide"
-//        const divElements = await page.$$eval('div.swiper-slide', elements => {
-//            return elements.filter(element => {
-//                // Check if the div element has only one child element which is a span element
-//                if (element.childNodes.length === 1 && element.childNodes[0].nodeName.toLowerCase() === 'span'){
-//                    console.log(element)
-//                }
-//                return element.childNodes.length === 1 && element.childNodes[0].nodeName.toLowerCase() === 'span';
-//            }).map(element => element.outerHTML);
-//        });
-//
-//
-//        // Loop through each div element and click on it
-//        for (const divElement of divElements) {
-//            console.log(`Clicking on "${divElement}"`)
-//            const parentDivElement = await divElement.$('..');
-//            await parentDivElement.click();
-//            await page.waitForSelector('.title');
-//
-//            // Get the movie titles
-//            const currentMovies = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim()));
-//            movieTitles.push(...currentMovies);
-//            console.log(currentMovies);
-//        }
-//    }
 
     // Clean the titles from "Neu:" and empty strings
     const cleanTitles = movieTitles.map(title => title.replace('Neu:', ''));
