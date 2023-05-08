091089fa335f0acc5cfafb9a4f655a1d1cd9a9b5
Leon Görgen <leon.goergen@gmail.com>
2023-05-08 18:22:17 +0200

    some refactoring

--- app.js
+++ app.js
@@ -1,58 +1,79 @@
 const puppeteer = require('puppeteer');
-const {ConsoleMessage} = require("puppeteer");
 
 (async () => {
-    // Launch a headless Chrome browser
-    const browser = await puppeteer.launch({headless: false, slowMo: 100});
+    try {
+        // Launch a headless Chrome browser
+        const browser = await puppeteer.launch({ headless: false, slowMo: 20 });
 
-    // Open a new tab
-    const page = await browser.newPage();
+        // Open a new tab
+        const page = await browser.newPage();
 
-    // Navigate to the website
-    await page.goto('https://www.cinestar.de/kino-rostock-capitol#Kinoprogramm');
+        // Navigate to the website
+        await page.goto('https://www.cinestar.de/kino-rostock-capitol#Kinoprogramm');
 
-    // Click on Cookie button
-    await page.waitForSelector('.mmcm-btn.mmcm-btn-save-all');
-    const [cookieButton] = await page.$$('.mmcm-btn.mmcm-btn-save-all');
-    await cookieButton.click();
-    await page.waitForNavigation();
+        // Click on Cookie button
+        await page.waitForSelector('.mmcm-btn.mmcm-btn-save-all');
+        const cookieButton = await page.$('.mmcm-btn.mmcm-btn-save-all');
+        await cookieButton.click();
+        await page.waitForNavigation();
 
-    // Try to get the buttons "Woche", "Heute", "Top10", ...
-    let sections = ["Heute", "Vorverkauf", "TOP 10", "Events", "Vorschau"]
-    let movieTitles = [];
-    for (let i = 0; i < sections.length; i++) {
-        let section = sections[i];
-        const [element] = await page.$x(`//span[contains(text(), '${section}')]`);
-        if (element) {
-            await element.click();
-            const currentMovies = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim()));
-            movieTitles.push(...currentMovies);
-        } else {
-            console.log("element not found");
+        // Define the sections to scrape
+        const sections = ['Heute', 'Vorverkauf', 'TOP 10', 'Events', 'Vorschau'];
+
+        // Scrape the movie titles for each section
+        const movieTitles = [];
+        for (const section of sections) {
+            const [element] = await page.$x(`//span[contains(text(), "${section}")]`);
+            if (element) {
+                await element.click();
+                await page.waitForSelector('.title');
+                const currentMovies = await page.$$eval('.title', elements => elements.map(element => element.textContent.trim().replace(/^Neu:\s*/, '')));
+                movieTitles.push(...currentMovies);
+            } else {
+                console.log(`Section "${section}" not found`);
+            }
         }
+
+        // Filter out duplicates and empty strings
+        const uniqueTitles = [...new Set(movieTitles.filter(title => title && title !== 'MEINE CineStarCARD' && title !== 'Passwort wiederherstellen'))];
+
+        // Clean the titles from noise
+        const cleanTitles = uniqueTitles.map(title => {
+            // Remove leading numbers and spaces
+            title = title.replace(/^\d+\s*/, '');
+            // Remove Cinestar prefixes
+            title = title.replace(/^Jeden \d+\.\s*/, '');
+            title = title.replace(/^CineLady-Preview/, '');
+            // Remove "(OV)" suffix
+            title = title.replace(/\(OV\)$/, '');
+            // Remove dates
+            title = title.replace(/^Am \d+\.\d+\.:\s*/, '');
+            // Remove other suffixes
+            title = title.replace(/–.*$/, '');
+            // Trim leading and trailing whitespace
+            title = title.trim();
+            return title;
+        });
+
+        // Sort out duplicates and empty strings
+        const uniqueCleanTitles = cleanTitles.filter((title, index) => {
+            return (
+                title !== 'MEINE CineStarCARD' &&
+                title !== 'Passwort wiederherstellen' &&
+                title !== 'OV CineSneak: Surprise, Surprise!' &&
+                title !== 'Montag: CineSneak - die Überraschungspreview' &&
+                title !== '' &&
+                index === cleanTitles.indexOf(title)
+            );
+        });
+
+        uniqueCleanTitles.sort();
+        console.log(`Found ${uniqueCleanTitles.length} unique movie titles:\n${JSON.stringify(uniqueCleanTitles, null, 2)}`);
+        //console.log(uniqueCleanTitles);
+
+        // Close the browser
+        await browser.close();
+    } catch (error) {
+        console.error(error);
     }
-
-    // Clean the titles from "Neu:"
-    const cleanTitles = movieTitles.map(title => title.replace('Neu:', ''));
-
-    // Sort out duplicates
-    const uniqueTitles = cleanTitles.filter((title, index) => {
-        return (
-            title !== 'MEINE CineStarCARD' &&
-            title !== 'Passwort wiederherstellen' &&
-            index === cleanTitles.indexOf(title)
-        );
-    });
-
-    // Sort out empty strings
-    if (uniqueTitles[uniqueTitles.length - 1] === '') {
-        uniqueTitles.pop();
-    }
-
-    console.log("Cleaned Movie Titles:")
-    console.log(uniqueTitles);
-    console.log(uniqueTitles.length)
-
-    // Close the browser
-    await browser.close();
 })();
