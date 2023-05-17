3ded6144b84bbc2b87b9ef302d0cbb3d808d211d
Leon Görgen <leon.goergen@gmail.com>
2023-05-17 16:04:53 +0200

    refactoring by putting everything in functions

--- app.js
+++ app.js
@@ -1,68 +1,95 @@
 const puppeteer = require('puppeteer');
 
+async function launchBrowser() {
+    // Launch a headless Chrome browser
+    const browser = await puppeteer.launch({ headless: false, slowMo: 5 });
+    return browser;
+}
+
+async function openPage(browser) {
+    // Open a new tab
+    const page = await browser.newPage();
+    return page;
+}
+
+async function navigateToWebsite(page) {
+    // Navigate to the website
+    await page.goto('https://www.cinestar.de/kino-rostock-capitol#Kinoprogramm');
+}
+
+async function clickCookieButton(page) {
+    // Click on Cookie button
+    await page.waitForSelector('.mmcm-btn.mmcm-btn-save-all');
+    const cookieButton = await page.$('.mmcm-btn.mmcm-btn-save-all');
+    await cookieButton.click();
+    await page.waitForNavigation();
+}
+
+async function scrapeMovieTitles(page) {
+    // Define the sections to scrape
+    const sections = ['Woche', 'Heute', 'Vorverkauf', 'TOP 10', 'Events', 'Vorschau'];
+
+    // Scrape the movie titles and genres for each section
+    const movies = [];
+    for (const section of sections) {
+        const [element] = await page.$x(`//span[contains(text(), "${section}")]`);
+        if (element) {
+            await element.click();
+            await page.waitForSelector('.ShowTile');
+            const sectionMovies = await page.$$eval('.ShowTile', elements => {
+                return elements.map(element => {
+                    const titleElement = element.querySelector('.title');
+                    const genreElement = element.querySelector('small');
+                    const title = titleElement ? titleElement.textContent.trim() : '';
+                    const genre = genreElement ? genreElement.textContent.trim() : '';
+                    return { title, genre };
+                });
+            });
+            movies.push(...sectionMovies);
+        } else {
+            console.log(`Section "${section}" not found`);
+        }
+    }
+    return movies;
+}
+
+async function filterMoviesByGenre(movies) {
+    // Filter movies by specific genres
+    const specificGenres = ['Event', 'Live-Übertragung', 'Sondervorstellung', 'Vorpremiere'];
+    const filteredMovies = movies.filter(movie => !specificGenres.includes(movie.genre));
+    return filteredMovies;
+}
+
+async function extractUniqueTitles(filteredMovies) {
+    // Extract unique movie titles after cleaning
+    const uniqueTitles = [...new Set(filteredMovies.map(movie => {
+        let title = movie.title.trim();
+        // Remove leading numbers and spaces
+        title = title.replace(/^\d+\s*/, '');
+        // Clean the titles from "Neu:"
+        title = title.replace(/^Neu:\s*/, '');
+        return title;
+    }))];
+
+    // Sort the clean titles
+    uniqueTitles.sort();
+
+    return uniqueTitles;
+}
+
 (async () => {
     try {
-        // Launch a headless Chrome browser
-        const browser = await puppeteer.launch({ headless: false, slowMo: 5 });
-
-        // Open a new tab
-        const page = await browser.newPage();
-
-        // Navigate to the website
-        await page.goto('https://www.cinestar.de/kino-rostock-capitol#Kinoprogramm');
-
-        // Click on Cookie button
-        await page.waitForSelector('.mmcm-btn.mmcm-btn-save-all');
-        const cookieButton = await page.$('.mmcm-btn.mmcm-btn-save-all');
-        await cookieButton.click();
-        await page.waitForNavigation();
-
-        // Define the sections to scrape
-        const sections = ['Woche', 'Heute', 'Vorverkauf', 'TOP 10', 'Events', 'Vorschau'];
-
-        // Scrape the movie titles and genres for each section
-        const movies = [];
-        for (const section of sections) {
-            const [element] = await page.$x(`//span[contains(text(), "${section}")]`);
-            if (element) {
-                await element.click();
-                await page.waitForSelector('.ShowTile');
-                const sectionMovies = await page.$$eval('.ShowTile', elements => {
-                    return elements.map(element => {
-                        const titleElement = element.querySelector('.title');
-                        const genreElement = element.querySelector('small');
-                        const title = titleElement ? titleElement.textContent.trim() : '';
-                        const genre = genreElement ? genreElement.textContent.trim() : '';
-                        return { title, genre };
-                    });
-                });
-                movies.push(...sectionMovies);
-            } else {
-                console.log(`Section "${section}" not found`);
-            }
-        }
-
-        // Filter movies by specific genres
-        const specificGenres = ['Event', 'Live-Übertragung', 'Sondervorstellung', 'Vorpremiere'];
-        const filteredMovies = movies.filter(movie => !specificGenres.includes(movie.genre));
-
-        // Extract unique movie titles after cleaning
-        const uniqueTitles = [...new Set(filteredMovies.map(movie => {
-            let title = movie.title.trim();
-            // Remove leading numbers and spaces
-            title = title.replace(/^\d+\s*/, '');
-            // Clean the titles from "Neu:"
-            title = title.replace(/^Neu:\s*/, '');
-            return title;
-        }))];
-
-        // Sort the clean titles
-        uniqueTitles.sort();
+        const browser = await launchBrowser();
+        const page = await openPage(browser);
+        await navigateToWebsite(page);
+        await clickCookieButton(page);
+        const movies = await scrapeMovieTitles(page);
+        const filteredMovies = await filterMoviesByGenre(movies);
+        const uniqueTitles = await extractUniqueTitles(filteredMovies);
 
         console.log(`Found ${uniqueTitles.length} unique movie titles:\n${JSON.stringify(uniqueTitles, null, 2)}`);
-        //console.log(uniqueTitles);
+        // console.log(uniqueTitles);
 
-        // Close the browser
         await browser.close();
     } catch (error) {
         console.error(error);
