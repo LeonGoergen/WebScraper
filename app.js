912f814480ca54bbf5fef6fd34e3a6b7dc4bfe0b
Leon Görgen <leon.goergen@gmail.com>
2023-05-22 19:11:27 +0200

    more refactoring + imdb and rotten tomatoes rating for every movie

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // Express Server
 const express = require('express')
 const app = express();
@@ -11,128 +12,168 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
 
 // Webscraper
 const puppeteer = require('puppeteer');
 
-async function launchBrowser() {
-    // Launch a headless Chrome browser
-    return await puppeteer.launch({headless: false, slowMo: 5});
-}
+class WebScraper {
+    constructor(url, headless = true, slowMo = 0) {
+        this.url = url;
+        this.browser = null;
+        this.page = null;
+        this.headless = headless;
+        this.slowMo = slowMo;
+    }
 
-async function openPage(browser) {
-    // Open a new tab
-    return await browser.newPage();
-}
+    async launchBrowser() {
+        this.browser = await puppeteer.launch({ headless: this.headless, slowMo: this.slowMo });
+    }
 
-async function navigateToWebsite(page) {
-    // Navigate to the website
-    await page.goto('https://www.cinestar.de/kino-rostock-capitol#Kinoprogramm');
-}
+    async openPage() {
+        this.page = await this.browser.newPage();
+    }
 
-async function clickCookieButton(page) {
-    // Click on Cookie button
-    await page.waitForSelector('.mmcm-btn.mmcm-btn-save-all');
-    const cookieButton = await page.$('.mmcm-btn.mmcm-btn-save-all');
-    await cookieButton.click();
-    await page.waitForNavigation();
-}
+    async navigateToWebsite() {
+        await this.page.goto(this.url);
+    }
 
-async function scrapeMovieTitles(page) {
-    const sections = ['Woche', 'Heute', 'Vorverkauf', 'TOP 10', 'Events', 'Vorschau'];
-    const movies = [];
+    async clickCookieButton() {
+        await this.page.waitForSelector('.mmcm-btn.mmcm-btn-save-all');
+        const cookieButton = await this.page.$('.mmcm-btn.mmcm-btn-save-all');
+        await cookieButton.click();
+        await this.page.waitForNavigation();
+    }
 
-    for (const section of sections) {
-        const element = await findSectionElement(page, section);
+    async scrapeMoviesFromSections(sections) {
+        let movies = [];
+        for (const section of sections) {
+            const element = await this.findSectionElement(section);
+            if (element) {
+                await element.click();
+                await this.page.waitForSelector('.ShowTile');
+                const sectionMovies = await this.scrapeMoviesFromSection();
+                movies.push(...sectionMovies);
+            } else {
+                console.log(`Section "${section}" not found`);
+            }
+        }
+        return movies;
+    }
 
-        if (element) {
-            await element.click();
-            await page.waitForSelector('.ShowTile');
-            const sectionMovies = await scrapeSectionMovies(page);
-            movies.push(...sectionMovies);
-        } else {
-            console.log(`Section "${section}" not found`);
+    async findSectionElement(section) {
+        const [element] = await this.page.$x(`//span[contains(text(), "${section}")]`);
+        return element;
+    }
+
+    async scrapeMoviesFromSection() {
+        return await this.page.$$eval('.ShowTile', elements => {
+            return elements.map(element => {
+                const titleElement = element.querySelector('.title');
+                const genreElement = element.querySelector('small');
+                const additionalInfoElement = element.querySelector('.subline .date');
+                const title = titleElement ? titleElement.textContent.trim() : '';
+                const genre = genreElement ? genreElement.textContent.trim() : '';
+                const date = additionalInfoElement ? additionalInfoElement.textContent.trim() : '';
+                return { title, genre, date };
+            });
+        });
+    }
+
+    static filterMoviesByGenre(movies, genresToExclude) {
+        return movies.filter(movie => !genresToExclude.includes(movie.genre));
+    }
+
+    static extractUniqueTitlesAndGenres(filteredMovies) {
+        const uniqueMovies = filteredMovies.reduce((acc, movie) => {
+            let title = movie.title.trim().replace(/^\d+\s*/, '').replace(/^Neu:\s*/, '');
+
+            const existingMovie = acc.find(m => m.title === title);
+            if (!existingMovie) {
+                acc.push({ title, genre: movie.genre });
+            }
+            return acc;
+        }, []);
+
+        uniqueMovies.sort((a, b) => a.title.localeCompare(b.title));
+
+        return uniqueMovies;
+    }
+
+    async getMovieRatings(movieTitle, key, lang) {
+        try {
+            const searchResponse = await fetch(`https://imdb-api.com/${lang}/API/SearchMovie/${key}/${encodeURIComponent(movieTitle)}`, {method: 'GET', redirect: 'follow'});
+            const searchData = await searchResponse.json();
+            const imdbId = searchData.results[0].id;
+
+            const ratingsResponse = await fetch(`https://imdb-api.com/${lang}/API/Ratings/${key}/${imdbId}`, {method: 'GET', redirect: 'follow'});
+            const ratingsData = await ratingsResponse.json();
+
+            console.log(ratingsData);
+
+            return {
+                imdbRating: ratingsData.imDb,
+                rottenTomatoesRating: ratingsData.rottenTomatoes
+            };
+        } catch (error) {
+            console.error(`Error fetching ratings for movie "${movieTitle}":`, error);
         }
     }
 
-    return movies;
-}
-
-async function findSectionElement(page, section) {
-    const [element] = await page.$x(`//span[contains(text(), "${section}")]`);
-    return element;
-}
-
-async function scrapeSectionMovies(page) {
-    return await page.$$eval('.ShowTile', elements => {
-        return elements.map(element => {
-            const titleElement = element.querySelector('.title');
-            const genreElement = element.querySelector('small');
-            const title = titleElement ? titleElement.textContent.trim() : '';
-            const genre = genreElement ? genreElement.textContent.trim() : '';
-            return {title, genre};
-        });
-    });
-}
-
-
-async function filterMoviesByGenre(movies) {
-    // Filter movies by specific genres
-    const specificGenres = ['Event', 'Live-Übertragung', 'Sondervorstellung', 'Vorpremiere'];
-    return movies.filter(movie => !specificGenres.includes(movie.genre));
-}
-
-async function extractUniqueTitles(filteredMovies) {
-    // Extract unique movie titles after cleaning
-    const uniqueTitles = [...new Set(filteredMovies.map(movie => {
-        let title = movie.title.trim();
-        // Remove leading numbers and spaces
-        title = title.replace(/^\d+\s*/, '');
-        // Clean the titles from "Neu:"
-        title = title.replace(/^Neu:\s*/, '');
-        return title;
-    }))];
-
-    // Sort the clean titles
-    uniqueTitles.sort();
-
-    return uniqueTitles;
+    async closeBrowser() {
+        await this.browser.close();
+    }
 }
 
 (async () => {
     try {
-        const browser = await launchBrowser();
-        const page = await openPage(browser);
-        await navigateToWebsite(page);
-        await clickCookieButton(page);
-        const movies = await scrapeMovieTitles(page);
-        const filteredMovies = await filterMoviesByGenre(movies);
-        const uniqueTitles = await extractUniqueTitles(filteredMovies);
+        const sections = ['Woche', 'Heute', 'Vorverkauf', 'TOP 10', 'Events', 'Vorschau'];
+        const specificGenres = ['Event', 'Live-Übertragung', 'Sondervorstellung', 'Vorpremiere'];
+        const url = 'https://www.cinestar.de/kino-rostock-capitol#Kinoprogramm';
 
-        console.log(`Found ${uniqueTitles.length} unique movie titles:\n${JSON.stringify(uniqueTitles, null, 2)}`);
-        // console.log(uniqueTitles);
+        const scraper = new WebScraper(url, false, 5);
+        await scraper.launchBrowser();
+        await scraper.openPage();
+        await scraper.navigateToWebsite();
+        await scraper.clickCookieButton();
 
-        await browser.close();
+        const movies = await scraper.scrapeMoviesFromSections(sections);
+        const filteredMovies = WebScraper.filterMoviesByGenre(movies, specificGenres);
+        const uniqueTitlesAndGenres = WebScraper.extractUniqueTitlesAndGenres(filteredMovies);
 
-        // First Attempts to get movie rating from "The Whale"
-        fetch('https://imdb-api.com/de/API/SearchMovie/k_x53sp327/The Whale',{method: 'GET', redirect: 'follow'})
-            .then(response => response.json())
-            .then(data => {
-                // Assuming the first result has the required IMDb ID
-                let imdbId = data.results[0].id;
+        let apiKey = "k_3nmeydf9" // "k_x53sp327"
+        for (let movie of uniqueTitlesAndGenres) {
+            console.log("Get ratings for: " + movie.title)
+            const ratings = await scraper.getMovieRatings(movie.title, apiKey, "de");
+            movie.imdbRating = ratings.imdbRating;
+            movie.rottenTomatoesRating = ratings.rottenTomatoesRating;
+        }
 
-                // Step 2: Fetch ratings using the IMDb ID
-                fetch(`https://imdb-api.com/de/API/Ratings/k_x53sp327/${imdbId}`, {method: 'GET', redirect: 'follow'})
-                    .then(response => response.json())
-                    .then(ratingsData => {
-                        // Process the ratings data as needed
-                        console.log(ratingsData);
-                    })
-                    .catch(error => console.log('Error fetching ratings:', error));
-            })
-            .catch(error => console.log('Error searching movie:', error));
+        console.log(`Found ${uniqueTitlesAndGenres.length} unique movies:\n${JSON.stringify(uniqueTitlesAndGenres, null, 2)}`);
 
+        await scraper.closeBrowser();
     } catch (error) {
         console.error(error);
     }
 })();
+
+/*
+// First Attempts to get movie rating from "The Whale"
+fetch('https://imdb-api.com/de/API/SearchMovie/k_x53sp327/The Whale',{method: 'GET', redirect: 'follow'})
+    .then(response => response.json())
+    .then(data => {
+        // Assuming the first result has the required IMDb ID
+        let imdbId = data.results[0].id;
+
+        // Step 2: Fetch ratings using the IMDb ID
+        fetch(`https://imdb-api.com/de/API/Ratings/k_x53sp327/${imdbId}`, {method: 'GET', redirect: 'follow'})
+            .then(response => response.json())
+            .then(ratingsData => {
+                // Process the ratings data as needed
+                console.log(ratingsData);
+            })
+            .catch(error => console.log('Error fetching ratings:', error));
+    })
+    .catch(error => console.log('Error searching movie:', error));
+
+ */
