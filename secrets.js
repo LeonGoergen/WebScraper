27ebbb556845c2b555bbde897dcfaba4cac687f5
Jonathan Zelfel <jonathan.zelfel@artesa.de>
2023-06-19 09:30:15 +0200

    fix: secrets for everyone

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 export default {
-    "consumerKey":"KEY",
-    "consumerSecret":"KEY",
-    "tokenKey":"KEY",
-    "tokenSecret":"KEY"
+    "consumerKey":"Gw8R9u8bvREcQmThXTm6I3tgj",
+    "consumerSecret":"d2dfyE0VlryeGnSU10NtrIH55VzRvEdNIQlH3zte7CyobOaCHR",
+    "tokenKey":"1655124797402107905-0INZX7ChXNxs20FCU7RKTnWafXwJ4v",
+    "tokenSecret":"QTScf0ybOgL2bFt0FlgkoALon7sbKNrTd7FzJtFHaqb34"
 }
