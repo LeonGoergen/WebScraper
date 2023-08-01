42e0d5362219355723f5a4f4f1fe6f9a38c66f8a
Leon Görgen <106811384+LeonGoergen@users.noreply.github.com>
2023-08-01 17:53:51 +0200

    Update secrets.js

--- Script/secrets.js
+++ Script/secrets.js
@@ -1,13 +1,13 @@
 export default {
-    "consumerKey":"Gw8R9u8bvREcQmThXTm6I3tgj",
-    "consumerSecret":"d2dfyE0VlryeGnSU10NtrIH55VzRvEdNIQlH3zte7CyobOaCHR",
-    "tokenKey":"1655124797402107905-0INZX7ChXNxs20FCU7RKTnWafXwJ4v",
-    "tokenSecret":"QTScf0ybOgL2bFt0FlgkoALon7sbKNrTd7FzJtFHaqb34",
+    "consumerKey":"",
+    "consumerSecret":"",
+    "tokenKey":"",
+    "tokenSecret":"",
 
-    "redditUsername": 'CinemaWeb20',
-    "redditPassword": 'CinemaSoSe23',
-    "clientId": '1t4N00lg099IMNsNtMaJZw',
-    "clientSecret": 'g0yMH_20HUZ0tVtYP66WM8_uHqmmbQ',
+    "redditUsername": '',
+    "redditPassword": '',
+    "clientId": '',
+    "clientSecret": '',
 
-    "imdbApiKey": "k_r5zmhtbn"
+    "imdbApiKey": ""
 }
